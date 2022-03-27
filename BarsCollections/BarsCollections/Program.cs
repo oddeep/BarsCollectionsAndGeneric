5477b843a4e3fc205120f9c68e6ce5a80263f28a
oddeep <adip1713@gmail.com>
2022-03-27 16:40:15 +0300

    Сделаны задания по обобщениям и по коллекциям

---
+++ BarsCollections/BarsCollections/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BarsCollections
+{
+    class Entity
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public string Name { get; set; }
+    }
+    static class Test
+    {
+        static public Dictionary<int, List<Entity>> Work(List<Entity> entities)
+        {
+            var dict = new Dictionary<int, List<Entity>>();
+            foreach (Entity ent in entities)
+            {
+                if (!(dict.ContainsKey(ent.ParentId)))
+                {
+                    dict.Add(ent.ParentId, new List<Entity> { ent });
+                }
+                else
+                {
+                    dict[ent.ParentId].Add(ent);
+                }
+            }
+            return dict;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var MyList = new List<Entity> { 
+                new Entity { Id = 1, ParentId = 0, Name = "Root entity"}, 
+                new Entity { Id = 2, ParentId = 1, Name = "Child of 1 entity"},
+                new Entity { Id = 3, ParentId = 1, Name = "Child of 1 entity"},
+                new Entity { Id = 4, ParentId = 2, Name = "Child of 2 entity"},
+                new Entity { Id = 5, ParentId = 4, Name = "Child of 4 entity"} 
+            };
+
+            var Diction = Test.Work(MyList);
+
+            //Печать элементов полученного словаря
+            foreach(var dict in Diction)
+            {
+                Console.Write($"Key = {dict.Key}, Value = {{");
+                foreach(var ent in dict.Value)
+                {
+                    Console.Write($"Entity{{Id = {ent.Id}}}, ");
+                }
+                Console.WriteLine("\b\b}");
+            }
+
+        }
+    }
+}
---
+++ BarsGeneric/BarsGeneric/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace BarsGeneric
+{
+    interface ILogger
+    {
+        void LogInfo(string message);
+        void LogWarning(string message);
+        void LogError(string message, Exception ex);
+    }
+
+    class LocalFileLogger<T> : ILogger
+    {
+        private string _filePath;
+        public LocalFileLogger(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void LogError(string message, Exception ex)
+        {
+            File.AppendAllText(_filePath, $"[Error] : [{typeof(T).Name}] : {message}.{ex.Message}\n");
+        }
+
+        public void LogInfo(string message)
+        {
+            File.AppendAllText(_filePath, $"[Info] : [{typeof(T).Name}] : {message}\n");
+        }
+
+        public void LogWarning(string message)
+        {
+            File.AppendAllText(_filePath, $"[Warning]  : [{typeof(T).Name}] : {message}\n");
+        }
+    }
+
+    class EngineMaker
+    {
+        public int Power { get; set; }
+    }
+
+    class Compukter
+    {
+        public int Model { get; set; }
+    }
+
+    class Person
+    {
+        public string Name
+        {
+            get; set;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var personLogger = new LocalFileLogger<Person>(@"C:\app\log.txt");
+            var compLogger = new LocalFileLogger<Compukter>(@"C:\app\log.txt");
+            var engineLogger = new LocalFileLogger<EngineMaker>(@"C:\app\log.txt");
+
+            personLogger.LogInfo("Информационное сообщение");
+            personLogger.LogWarning("Предупреждение!");
+            personLogger.LogError("Произошла ошибка", new SystemException());
+
+            compLogger.LogInfo("Информационное сообщение");
+            compLogger.LogWarning("Предупреждение!");
+            compLogger.LogError("Произошла ошибка", new SystemException());
+
+            engineLogger.LogInfo("Информационное сообщение");
+            engineLogger.LogWarning("Предупреждение!");
+            engineLogger.LogError("Произошла ошибка", new SystemException());
+        }
+    }
+}
